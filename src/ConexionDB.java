0d2d377b1cfd12ad1d537fba58c29be3349f6b33
Fierro-z <carlosfierropozo@gmail.com>
2026-05-11 11:12:08 -0500

    feat: implement SQLite connection and database initialization for VetSentinel

--- src/ConexionDB.java
+++ src/ConexionDB.java
@@ -4,8 +4,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.List;
 
 public class ConexionDB {
 
